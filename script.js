6aa8b4d9cbe992585e8e95629fb0ac28a78d4481
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-07-07 14:44:24 -0300

    atualização JS

--- script.js
+++ script.js
@@ -23,3 +23,20 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log("Modelo Carregado!");
 }
+
+function check() {
+    img = document.getElementById("captured_image")
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(result)
+        document.getElementById("resultObjectName").innerHTML = result[0].label;
+        document.getElementById("resultObjectAccuracy").innerHTML = result[0].confidence.toFixed(3); 
+    }
+
+};
